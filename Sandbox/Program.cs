4592e6fa96dbcad4d431af5c8d5a9d4ef9db0e74
Colin Dabritz <colin@dabritz.org>
2011-06-08 00:46:56 -0700

    Example programing handling constraints on stacked values.

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -5,11 +5,132 @@ using System.Text;
 
 namespace Sandbox
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Modifier ArmorClass = new Modifier()
+            {
+                Name = "Misc",
+                Base = 1,
+                innerModifier = new Modifier()
+                {
+                    Name = "Dex",
+                    Base = 5,
+                    innerModifier = new ConstrainingModifier()
+                    {
+                        Name = "Shield",
+                        Base = 5,
+                        OverrideName = "Dex",
+                        MaxOverrideValue = 2,
+                        innerModifier = new Modifier()
+                        {
+                            Name = "Magic Armor",
+                            Base = 4,
+                            innerModifier = new ConstrainingModifier()
+                            {
+                                Name = "Armor",
+                                Base = 8,
+                                OverrideName = "Dex",
+                                MaxOverrideValue = 3,
+                                innerModifier = new Modifier()
+                                {
+                                    Name = "AC",
+                                    Base = 10
+                                }
+                            }
+                        }
+                    }
+
+                }
+            };
+
+            Console.WriteLine(ArmorClass.Value);
+
         }
     }
+
+    public class Modifier
+    {
+        public string Name { get; set; }
+        public int Base { get; set; }
+
+        public Modifier innerModifier;
+
+        public int GetConstrainedValue()
+        {
+            if (innerModifier != null)
+            {
+                return innerModifier.GetConstrainedValue(Name, Base);
+            }
+            else
+            {
+                return Base;
+            }
+        }
+
+        public virtual int GetConstrainedValue(string name, int value)
+        {
+            if (innerModifier != null)
+            {
+                return innerModifier.GetConstrainedValue(name, value);
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public int Value
+        {
+            get
+            {
+                if (innerModifier != null)
+                {
+                    return GetConstrainedValue() + innerModifier.Value;
+                }
+                else
+                {
+                    return GetConstrainedValue();
+                }
+            }
+        }
+    }
+
+    public class ConstrainingModifier : Modifier
+    {
+        public string OverrideName { get; set; }
+        public int MaxOverrideValue { get; set; }
+
+        public override int GetConstrainedValue(string name, int value)
+        {
+            if (name == OverrideName)
+            {
+                if (innerModifier != null)
+                {
+                    return Math.Min(MaxOverrideValue, innerModifier.GetConstrainedValue(name, value));
+                }
+                else
+                {
+                    return Math.Min(MaxOverrideValue, value);
+                }
+            }
+            else
+            {
+                if (innerModifier != null)
+                {
+                    return innerModifier.GetConstrainedValue(name, value);
+                }
+                else
+                {
+                    return value;
+                }
+            }
+
+        }
+    }
+
+
 }
